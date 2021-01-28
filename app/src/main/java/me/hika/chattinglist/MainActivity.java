7cf8a4b0d37e438ff24377c67b19c481cb317385
Abdul Fatah <fatahhikami@gmail.com>
2021-01-28 11:36:23 +0700

    Call Adapter

--- app/src/main/java/me/hika/chattinglist/MainActivity.java
+++ app/src/main/java/me/hika/chattinglist/MainActivity.java
@@ -1,14 +1,33 @@
 package me.hika.chattinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import me.hika.chattinglist.adapter.MessageAdapter;
+import me.hika.chattinglist.model.MessageModel;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        ArrayList<MessageModel> messagesList = new ArrayList<>();
+        for (int i=0;i<10;i++) {
+            messagesList.add(new MessageModel("Hi", i % 2 == 0 ? MessageAdapter.MESSAGE_TYPE_IN : MessageAdapter.MESSAGE_TYPE_OUT));
+        }
+
+        MessageAdapter adapter = new MessageAdapter(messagesList, this);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 }
